836f45850e1e8a43adfa63cc29173a8b1edfcb56
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-02 12:03:29 +1000

    New file to prepare trees

---
+++ code/00_prepare_trees.R
@@ -0,0 +1,8 @@
+# ancient_ILS/code/00_prepare_trees.R
+## This script prepares the three hypothesis trees for the animal tree of life
+# Caitlin Cherryh, 2022
+
+#### 1. Input parameters ####
+# repo_dir            <- Location of caitlinch/ancient_ILS github repository
+
+repo_dir <- "/Users/caitlincherryh/Documents/Repositories/ancient_ILS/"
