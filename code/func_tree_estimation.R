8ef488362e7bff76efc096ab4e6e1e0e4e65364e
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-04 16:01:12 +1000

    Added code to calculate gCF for each simulated alignment.

--- code/func_tree_estimation.R
+++ code/func_tree_estimation.R
@@ -70,7 +70,7 @@ estimate.one.tree <- function(alignment_path, unique.output.path = TRUE, iqtree2
   output_tree_file <- paste0(dirname(alignment_path), output_prefix, ".treefile")
   output_iqtree_file <- paste0(dirname(alignment_path), output_prefix, ".iqtree")
   output_log_file <- paste0(dirname(alignment_path), output_prefix, ".log")
-
+  
   # Assemble output
   output_vec <- c(alignment_path, output_prefix, output_model, iqtree2_call, run.iqtree2, output_tree_file, output_iqtree_file, output_log_file)
   names(output_vec) <- c("alignment_path", "alignment_model_ID", "tree_estimation_model", "iqtree2_command", "iqtree2_command_run", "ML_tree_treefile", "ML_tree_iqtree_file", "ML_tree_log_file")
@@ -78,3 +78,79 @@ estimate.one.tree <- function(alignment_path, unique.output.path = TRUE, iqtree2
 }
 
 
+iqtree2.concordance.factors <- function(alignment_path, iqtree2_path, iqtree2_num_threads = "AUTO", iqtree2_num_ufb = 1000,
+                                        iqtree2_model = NA){
+  # Function to take a simulated alignment and estimate gCF from it using iqtree2
+  
+  ## Get the directory and list of files in that directory
+  al_dir <- paste0(dirname(alignment_path),"/")
+  al_files <- list.files(al_dir)
+  # Change to that dorectory
+  setwd(al_dir)
+  # Extract the unique id for this alignment
+  al_id <- tail(unlist(strsplit(al_dir, "/")),1)
+  
+  ## Create a gene partition file with no models
+  # Find and open the alisim partition file
+  alisim_partition_file <- paste0(al_dir, grep("log", grep("partition", al_files, value = TRUE), value = TRUE, invert = TRUE))
+  # Generate the gcf partition file
+  gcf_partition_file <- generate.gcf.partition.file(alisim_partition_file)
+  
+  ## Inferring species tree
+  # Create model call
+  if (is.na(iqtree2_model) == TRUE){
+    model_call <- " -m MFP+MERGE "
+  } else if (is.na(iqtree2_model) == FALSE){
+    model_call <- paste0(" -m ", iqtree2_model, " ")
+  }
+  # Create IQ-Tree call
+  species_tree_prefix <- paste0(al_id, "-concat")
+  species_tree_call <- paste0(iqtree2_path, " -s ", alignment_path, " -p ", gcf_partition_file, model_call, " --prefix ", species_tree_prefix, " -bb ",  iqtree2_num_ufbm, " -nt ",iqtree2_num_threads)
+  system(species_tree_call)
+  
+  ## Inferring gene/locus trees  
+  # Create model call
+  if (is.na(iqtree2_model) == TRUE){
+    model_call <- " -m MFP+MERGE "
+  } else if (is.na(iqtree2_model) == FALSE){
+    model_call <- paste0(" -m ", iqtree2_model, " ")
+  }
+  # Create IQ-Tree call
+  gene_tree_prefix <- paste0(al_id, "-gene_trees")
+  gene_tree_call <- paste0(iqtree2_path, " -s ", alignment_path, " -S ", gcf_partition_file, model_call, " --prefix ", gene_tree_prefix, " -bb ",  iqtree2_num_ufbm, " -nt ",iqtree2_num_threads)
+  system(gene_tree_call)
+  
+  ## Calculating gene concordance factors
+  gcf_tree_prefix <- paste0(al_id, "-concord")
+  gcf_call <- paste0(iqtree2_path ," -t ", species_tree_prefix, ".treefile --gcf ", gene_tree_prefix, ".treefile --prefix ", gcf_tree_prefix)
+  system(gcf_call)
+  
+  ## Return output
+  
+}
+
+
+#### Functions for partition files ####
+generate.gcf.partition.file <- function(partition_file){
+  # Quick function to create a gCF partition file from an Alisim partition file 
+  
+  # Open partition file
+  alisim_partitions <- readLines(partition_file)
+  # Extract gene names
+  gene_names <- grep("gene", unlist(strsplit(grep("charset", alisim_partitions, value = T), " ")), value = T)
+  # Make a new charpartition section
+  new_charpartition <- paste0("\tcharpartition genes = ", paste(gene_names, collapse = ", "), ";")
+  # Replace the charpartition section
+  line_ind <- grep("charpartition", alisim_partitions)
+  alisim_partitions[line_ind] <- new_charpartition
+  # Create file path
+  p_dir <- paste0(dirname(partition_file), "/")
+  p_id <- tail(unlist(strsplit(p_dir, "/")),1)
+  gcf_partitions <- paste0(p_dir, p_id, "_gCF_partitions.nexus")
+  # Save the new partition file 
+  write(alisim_partitions, file = gcf_partitions)
+  # Return the gcf partition file path
+  return(gcf_partitions)
+}
+  
+  
