f01fc97429ed33972898aceef5e808ddd2155721
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-04 16:21:01 +1000

    Added function to calculate actual gCFS from gene trees/ms tree

--- code/func_tree_estimation.R
+++ code/func_tree_estimation.R
@@ -78,6 +78,53 @@ estimate.one.tree <- function(alignment_path, unique.output.path = TRUE, iqtree2
 }
 
 
+#### Concordance factors ####
+extract.input.concordance.factors <- function(alignment_path, iqtree2_path, iqtree2_num_threads = "AUTO"){
+  # Function to return concordance factors (calculated in iqtree), for a simulated tree
+  #     gCFs estimated from the base tree supplied to ms and from the set of gene trees estimated in AliSim
+  
+  ## Get the directory and list of files in that directory
+  al_dir <- paste0(dirname(alignment_path),"/")
+  al_files <- list.files(al_dir)
+  # Change to that dorectory
+  setwd(al_dir)
+  # Extract the unique id for this alignment
+  al_id <- tail(unlist(strsplit(al_dir, "/")),1)
+  
+  ## Identify the files for the starting tree and the gene trees
+  tree_file <- paste0(al_dir, grep("branch_lengths_modified", al_files, value = T))
+  gene_tree_file <- paste0(al_dir, grep("ms_gene_trees", al_files, value = T))
+  
+  ## Calculate gCFS
+  actual_gcf_prefix <- paste0(al_id, "-actual")
+  actual_gcf_call <- paste0(iqtree2_path ," -t ", tree_file, " --gcf ", gene_tree_file, " --prefix ", actual_gcf_prefix)
+  system(actual_gcf_call)
+  
+  ## Extract and return gCFs
+  # Open the gcf table
+  al_files <- paste0(al_dir, list.files(al_dir))
+  gcf_files <- grep(actual_gcf_prefix, al_files, value = T)
+  gcf_stat_file = grep("cf.stat", gcf_files, value = T)
+  gcf_table <- read.table(gcf_stat_file, header = TRUE, sep = "\t")
+  # Return the gCF table
+  return(gcf_table)
+}
+
+extract.output.concordance.factors <- function(alignment_path, iqtree2_path, iqtree2_num_threads = "AUTO", iqtree2_num_ufb = 1000,
+                                        iqtree2_model = NA){
+  # Function to return concordance factors (calculated in iqtree), for a simulated alignment
+  #     gCFs estimated from the ML tree estimated from the partitioned simulated alignment and gene trees estimated from each partition
+  
+  # Run iqtree to estimate gene concordance factors
+  gcf_files <- iqtree2.concordance.factors(alignment_path, iqtree2_path, iqtree2_num_threads, iqtree2_num_ufb, iqtree2_model)
+  # Retrieve gcf from output table
+  gcf_stat_file <- op_vector[["gCF_table_file"]]
+  gcf_table <- read.table(gcf_stat_file, header = TRUE, sep = "\t")
+  # Return the gCF table
+  return(gcf_table)
+  
+}
+
 iqtree2.concordance.factors <- function(alignment_path, iqtree2_path, iqtree2_num_threads = "AUTO", iqtree2_num_ufb = 1000,
                                         iqtree2_model = NA){
   # Function to take a simulated alignment and estimate gCF from it using iqtree2
@@ -126,7 +173,14 @@ iqtree2.concordance.factors <- function(alignment_path, iqtree2_path, iqtree2_nu
   system(gcf_call)
   
   ## Return output
-  
+  # Assemble output files
+  gcf_tree_file <- paste0(al_dir, gcf_tree_prefix, ".cf.tree")
+  gcf_branch_file <- paste0(al_dir, gcf_tree_prefix, ".cf.branch")
+  gcf_table_file <- paste0(al_dir, gcf_tree_prefix, ".cf.stat")
+  # Assemble output
+  op_vector <- c(alignment_path, gcf_tree_file, gcf_branch_file, gcf_table_file)
+  names(op_vector) <- c("alignment_path", "gCF_tree_file", "gCF_branch_file", "gCF_table_file")
+  return(op_vector)
 }
 
 
