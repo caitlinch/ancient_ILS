8d9504f2014d9506d1eeef8123acaecf92d2dfc6
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-04 12:33:03 +1000

    New file for functions for tree estimation.

---
+++ code/func_tree_estimation.R
@@ -0,0 +1,11 @@
+# ancient_ILS/code/func_tree_estimation.R
+## This script includes functions to run tree estimation for this project
+# Caitlin Cherryh, 2023
+
+
+#### Functions for IQ-Tree2 ####
+estimate.one.tree <- function(alignment_path, gene_models, iqtree2){
+  
+}
+
+
