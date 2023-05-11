e2adebc010c919106376bca044e6aa5e0b26da16
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-11 18:06:01 +1000

    Added function to return the length of each gene in an smatrix.txt file. Prettier formatting for all functions.

--- code/func_prepare_trees.R
+++ code/func_prepare_trees.R
@@ -3,8 +3,7 @@
 # Caitlin Cherryh, 2023
 
 format.constraint.tree.clade <- function(clade){
-  # Function to take in a vector of species and return a nicely formatted character object to paste into a constraint tree
-  
+  ## Function to take in a vector of species and return a nicely formatted character object to paste into a constraint tree
   # Check how many taxa are in the clade
   clade_size <- length(clade)
   
@@ -23,8 +22,7 @@ format.constraint.tree.clade <- function(clade){
 
 
 partition.one.model.line <- function(model_line){
-  # Small function to take one line from the models file and return in partition format
-  
+  ## Small function to take one line from the models file and return in partition format
   # Break line into two chunks
   line_break <- strsplit(model_line, "\\=")
   model_chunk <- line_break[[1]][1]
@@ -37,10 +35,11 @@ partition.one.model.line <- function(model_line){
 }
 
 
+
+
 #### Reading gene lengths from partition files ####
 gene.lengths.nexus <- function(partition_file){
-  # Extract all gene lengths from a nexus partition file 
-  
+  ## Extract all gene lengths from a nexus partition file
   # Open partition file
   lines <- readLines(partition_file)
   # Extract all lines with a charset
@@ -61,9 +60,9 @@ gene.lengths.nexus <- function(partition_file){
   return(gene_df$gene_length)
 }
 
+
 gene.lengths.raxml <- function(partition_file){
-  # Extract all gene lengths from a nexus partition file 
-  
+  ## Extract all gene lengths from a RAxML partition file 
   # Open partition file
   lines <- readLines(partition_file)
   # Extract all lines with an equals sign (these lines will define a gene)
@@ -75,6 +74,28 @@ gene.lengths.raxml <- function(partition_file){
   # Get start and end of each gene
   gene_start <- as.numeric(unlist(lapply(strsplit(gene_chunks_nospace, "-"), function(x){x[1]})))
   gene_end <- as.numeric(unlist(lapply(strsplit(gene_chunks_nospace, "-"), function(x){x[2]})))
+  # Create a nice little dataframe for the genes
+  gene_df <- data.frame(gene_range = gene_chunks_nospace, gene_start = gene_start, gene_end = gene_end)
+  # Calculate the gene length
+  gene_df$gene_length <- gene_end - (gene_start - 1) # subtract one from gene_start to count the starting site in the gene length
+  # Return the gene length
+  return(gene_df$gene_length)
+}
+
+
+gene.lengths.smatrix <- function(partition_file){
+  ## Extract all gene lengths from an smatrix.txt file 
+  # Open file
+  lines <- readLines(partition_file)
+  # Extract all lines with arrow "=>" (these lines will define a gene)
+  arrow_lines <- grep("\\=>", lines, value = TRUE)
+  # Split the charset lines at the tab "\t"
+  gene_chunks <- unlist(lapply(strsplit(arrow_lines, "\t"), function(x){x[1]}))
+  # Format the gene chunks nicely
+  gene_chunks_nospace <- gsub(" ", "", gene_chunks)
+  # Get start and end of each gene
+  gene_start <- as.numeric(unlist(lapply(strsplit(gene_chunks_nospace, "\\=>"), function(x){x[1]})))
+  gene_end <- as.numeric(unlist(lapply(strsplit(gene_chunks_nospace, "\\=>"), function(x){x[2]})))
   # Create a nice little dataframe for the genes
   gene_df <- data.frame(gene_range = gene_chunks_nospace, gene_start = gene_start, gene_end = gene_end)
   # Calculate the gene length
