2cb9b95ad7ce8a5fcdc62e9dbcd6aec69805c4e4
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-11 18:44:47 +1000

    Make and output a nice histogram of gene lengths.

--- code/00_plot_gene_lengths.R
+++ code/00_plot_gene_lengths.R
@@ -1,5 +1,5 @@
 # ancient_ILS/code/00_plot_gene_lengths.R
-## This script prepares the three hypothesis trees for the animal tree of life
+## This script plots a histogram of gene lengths for 15 different empirical phylogenetic datasets (each designed to determine the relationship between Animal clades)
 # Caitlin Cherryh, 2023
 
 #### 1. Input parameters ####
@@ -13,6 +13,9 @@ partition_dir               <- "/Users/caitlincherryh/Documents/C4_Ancient_ILS/0
 
 
 #### 2. Open packages and functions ####
+## Source packages
+library(ggplot2)
+
 ## Source functions
 source(paste0(repo_dir, "code/func_prepare_trees.R"))
 
@@ -26,10 +29,24 @@ if (dir.exists(repo_output) == FALSE){
 
 
 
-#### 3. Extract gene lengths ####
+#### 3. Extract and plot gene lengths ####
 # Get all files from the partition folder
 all_files <- list.files(partition_dir)
-# Break into three chunks
-nexus_partitions <- grep("partitions.nex", all_files, value = TRUE)
-raxml_partitions <- grep("partitions.txt", all_files, value = TRUE)
-smatrix_partitions <- grep("smatrix.txt", all_files, value = TRUE)
+# Break into parts based on file type
+nexus_partitions    <- grep("partitions.nex", all_files, value = TRUE)
+raxml_partitions    <- grep("partitions.txt", all_files, value = TRUE)
+smatrix_partitions  <- grep("smatrix.txt", all_files, value = TRUE)
+txt_files           <- grep("gene_lengths.txt", all_files, value = TRUE)
+# Extract the gene lengths from the partition files
+nexus_gene_lengths      <- lapply(paste0(partition_dir, nexus_partitions), gene.lengths.nexus)
+raxml_gene_lengths      <- lapply(paste0(partition_dir, raxml_partitions), gene.lengths.raxml)
+smatrix_gene_lengths    <- lapply(paste0(partition_dir, smatrix_partitions), gene.lengths.smatrix)
+# Extract the gene lengths from the gene length text files
+txt_gene_lengths  <- lapply(paste0(partition_dir, txt_files), function(f){as.numeric(readLines(f))})
+# Make one really long vector with all the gene lengths
+gene_lengths <- c(unlist(nexus_gene_lengths), unlist(raxml_gene_lengths), 
+                  unlist(smatrix_gene_lengths), unlist(txt_gene_lengths))
+# Plot a cute little histogram of the gene lengths
+
+
+
