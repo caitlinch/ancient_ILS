5c288d49fcda45484ce785436a87b43610d06078
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-07-12 17:29:52 +1000

    Open ASTRAL tree and the three topologies of interest.

--- code/03_plot_trees.R
+++ code/03_plot_trees.R
@@ -11,23 +11,30 @@ repo_dir <- "/Users/caitlincherryh/Documents/Repositories/ancient_ILS/"
 
 #### 2. Open packages and prepare variables ####
 library(ape)
+library(ggtree)
 library(patchwork)
 
 
-#### 3. Plot phylogenetic hypotheses ####
-# Open the trees
-hyp_trees <- read.tree(file = paste0(repo_dir, "/trees/alternative_phylogenetic_hypotheses.nex"))
 
-# Plot possible topologies
-top_trees <- hyp_trees[1:3]
-par(mfrow=c(1,1))
-plot(hyp_trees[[1]], cex = 2, edge.width = 3)
-plot(hyp_trees[[2]], cex = 2, edge.width = 3)
-plot(hyp_trees[[3]], cex = 2, edge.width = 3)
-plot(hyp_trees[[4]], cex = 2, edge.width = 3)
-plot(hyp_trees[[5]], cex = 2, edge.width = 3)
-plot(hyp_trees[[6]], cex = 2, edge.width = 3)
-plot(hyp_trees[[7]], cex = 2, edge.width = 3)
-plot(hyp_trees[[8]], cex = 2, edge.width = 3)
-plot(hyp_trees[[9]], cex = 2, edge.width = 3)
+#### 4. Plot figures for introduction and methods ####
+# Identify the ASTRAL tree
+empirical_trees <- list.files(paste0(repo_dir, "empirical_tree"))
+astral_tree <- paste0(repo_dir, "empirical_tree/", grep("Whelan2017_ASTRAL_tree.tre", empirical_trees, value = T))
+# Open the ASTRAL tree
+a_tree <- read.tree(astral_tree)
+# Reroot tree
+a_tree <- root(a_tree, outgroup = c("Salpingoeca_pyxidium", "Monosiga_ovata", "Acanthoeca_sp", "Salpingoeca_rosetta", "Monosiga_brevicolis"), resolve.root = TRUE)
+# Set missing branch lengths to 2
+a_tree$edge.length[which(is.na(a_tree$edge.length))] <- 3
+
+
+#### 4. Plot phylogenetic hypotheses ####
+# Open the possible phylogenetic topologies
+hyp_trees <- read.tree(file = paste0(repo_dir, "hypothesis_trees/alternative_phylogenetic_hypotheses.nex"))
+c_tree <- hyp_trees[[1]]
+p_tree <- hyp_trees[[2]]
+cp_tree <- hyp_trees[[3]]
+
+
+
 
