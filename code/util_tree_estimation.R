75559356c6db63be64fb31cbf86786eb9ff6ba30
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-07-25 15:51:53 +1000

    New code to automatically generate Slurm files to estimate trees.

--- code/util_tree_estimation.R
+++ code/util_tree_estimation.R
@@ -12,7 +12,7 @@ location = "local"
 if (location == "local"){
   ## File paths and computational parameters
   repo_dir                    <- "/Users/caitlincherryh/Documents/Repositories/ancient_ILS/"
-  output_dir                  <- "/Users/caitlincherryh/Documents/C4_Ancient_ILS/03_simulations/01_simulation_output/"
+  output_dir                  <- "/Users/caitlincherryh/Documents/C4_Ancient_ILS/03_simulation_output/"
   iqtree2                     <- "iqtree2"
 } else if (location == "dayhoff"){
   ## File paths and computational parameters
@@ -23,32 +23,51 @@ if (location == "local"){
 
 
 #### 2. Open packages and functions ####
-## Source functions
-source(paste0(repo_dir, "code/func_prepare_trees.R"))
-
-# Create output file paths
-output_files <- paste0(output_dir, c("ancientILS_simulation_parameters.csv", "ancientILS_output_generate_alignments.csv",
-                                     "ancientILS_output_generate_trees_duplicateCols.csv", "ancientILS_output_generate_trees.csv",
-                                     "ancientILS_output_gCF.csv"))
-names(output_files) <- c("simulations", "alignments", "trees_duplicate_columns", "trees", "analysis")
+# Open the csv with tree estimation command lines
+tree_df <- read.csv(paste0(output_dir, grep("generate_trees", list.files(output_dir), value = T)))
 
 
 
-#### 3. Create new shorter tip names ####
-simulation_taxa_names <- paste0("t", 1:75)
-names(simulation_taxa_names) <- c("Homo_sapiens", "Strongylocentrotus_purpatus", "Hemithris_psittacea", "Capitella_teleta", "Drosophila_melanogaster","Daphnia_pulex",
-                                  "Hydra_vulgaris", "Bolocera_tuediae", "Aiptasia_pallida", "Hormathia_digitata", "Nematostella_vectensis", "Acropora_digitifera", 
-                                  "Eunicella_verrucosa", "Hydra_viridissima", "Hydra_oligactis", "Physalia_physalia", "Abylopsis_tetragona","Craseo_lathetica",
-                                  "Nanomia_bijuga", "Agalma_elegans", "Periphyla_periphyla", "Cliona_varians", "Sycon_coactum", "Sycon_ciliatum", "Corticium_candelabrum",
-                                  "Oscarella_carmela", "Hyalonema_populiferum", "Aphrocallistes_vastus", "Rossella_fibulata", "Sympagella_nux", "Ircinia_fasciculata",
-                                  "Chondrilla_nucula", "Amphimedon_queenslandica", "Petrosia_ficiformis", "Spongilla_lacustris", "Pseudospongosorites_suberitoides",
-                                  "Mycale_phylophylla", "Latrunculia_apicalis", "Crella_elegans", "Kirkpatrickia_variolosa", "Euplokamis_dunlapae", "Vallicula_sp",
-                                  "Coeloplana_astericola", "Hormiphora_californica", "Hormiphora_palmata", "Pleurobrachia_pileus", "Pleurobrachia_bachei",
-                                  "Pleurobrachia_sp_South_Carolina_USA", "Cydippida_sp_Maryland_USA", "Callianira_Antarctica", "Mertensiidae_sp_Antarctica",
-                                  "Mertensiidae_sp_Washington_USA", "Cydippida_sp", "Dryodora_glandiformis", "Lobatolampea_tetragona", "Beroe_abyssicola", "Beroe_sp_Antarctica",
-                                  "Beroe_ovata", "Beroe_sp_Queensland_Australia", "Beroe_forskalii", "Ocyropsis_sp_Bimini_Bahamas", "Ocyropsis_crystallina", "Ocyropsis_sp_Florida_USA",
-                                  "Bolinopsis_infundibulum", "Mnemiopsis_leidyi", "Bolinopsis_ashleyi", "Lobata_sp_Punta_Arenas_Argentina", "Eurhamphaea_vexilligera", "Cestum_veneris",
-                                  "Ctenophora_sp_Florida_USA","Salpingoeca_pyxidium", "Monosiga_ovata", "Acanthoeca_sp", "Salpingoeca_rosetta", "Monosiga_brevicolis")
+#### 3. Generate slurm files to run tree estimation ####
+# Remove any double spaces in command lines
+tree_df$iqtree2_gene_tree_command <- gsub("   ", "", tree_df$iqtree2_gene_tree_command)
+tree_df$iqtree2_gene_tree_command <- gsub("  ", "", tree_df$iqtree2_gene_tree_command)
+# Create template for slurm files
+slurm_header <- c("#!/bin/bash", 
+                  "#") 
+slurm_name <- c("#SBATCH --job-name=")
+slurm_body <- c("#SBATCH --output=/mnt/data/dayhoff/home/u5348329/ancient_ILS/slurm_files/%j.%x.out",
+                "#SBATCH --error=/mnt/data/dayhoff/home/u5348329/ancient_ILS/slurm_files/%j.%x.err",
+                "#SBATCH --partition=Standard",
+                "#",
+                "#SBATCH --time=144:00:00 # 6 days",
+                "#SBATCH --ntasks=1",
+                "#SBATCH --cpus-per-task=10",
+                "#SBATCH --mem=1000 # 1GB",
+                "#",
+                "#SBATCH --mail-user u5348329@anu.edu.au",
+                "#SBATCH --mail-type TIME_LIMIT,FAIL",
+                "",
+                "# Open conda environment",
+                "source /opt/conda/bin/activate",
+                "conda activate /mnt/data/dayhoff/home/u5348329/.conda/envs/ancient_ILS",
+                "",
+                "# Change to working directory for this project",
+                "cd /mnt/data/dayhoff/home/u5348329/ancient_ILS/03_simulation_output/",
+                "",
+                "# Estimate trees")
+slurm_footer <- c("")
+# Assemble code lines
+tree_df$combined_tree_estimation_command <- paste0("cd ", tree_df$output_folder, ";", tree_df$iqtree2_gene_tree_command, ";", tree_df$ASTRAL_command)
+# Loop and assemble scripts
+for (i in 1:8){
+  rows <- seq(from = ((i-1)*25)+1, to = (i*25), by = 1)
+  temp_slurm_file <- c(slurm_header, paste0(slurm_name, "te",i), slurm_body, tree_df$combined_tree_estimation_command[rows], slurm_footer)
+  temp_op_file <- paste0(output_dir, "te", i, ".sh")
+  write(temp_slurm_file, file = temp_op_file)
+}
+
+
 
 #### 4.Check which trees are unrun ####
 # Extract the list of all simulation replicates
@@ -65,9 +84,9 @@ print(paste0("Missing ML trees: ", length(which(check_df$ml_tree_complete == FAL
 incomplete_ids <- check_df[(check_df$ml_tree_complete == FALSE | check_df$gene_trees_complete == FALSE | check_df$astral_tree_complete == FALSE),]$id
 # Open dataframe of simulation parameters and trim to incomplete trees
 
-  
-  
-  
-  
-  
+
+
+
+
+
 
