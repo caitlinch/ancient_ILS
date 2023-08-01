74f573981944edd09cb715f7bc9e5c5c93d16cf0
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-08-01 15:31:15 +1000

    New file for plotting output figures.

---
+++ code/02_plot_figures.R
@@ -0,0 +1,37 @@
+# ancient_ILS/code/02_plot_figures.R
+## This script plots result figures from simulation output
+# Caitlin Cherryh, 2023
+
+#### 1. Input parameters ####
+## File paths
+# repo_dir                  <- location of caitlinch/ancient_ILS github repository
+# input_dir                 <- location of simulation and analysis result dataframes
+# output_dir                <- output directory to save figures
+
+location = "local"
+if (location == "local"){
+  ## File paths
+  repo_dir                    <- "/Users/caitlincherryh/Documents/Repositories/ancient_ILS/"
+  input_dir                   <- "/Users/caitlincherryh/Documents/C4_Ancient_ILS/03_simulation_output/"
+  output_dir                  <- "/Users/caitlincherryh/Documents/C4_Ancient_ILS/04_figures/"
+}
+
+
+
+#### 2. Open packages and functions ####
+## Source functions
+
+
+## Open packages
+library(ggplot2)
+
+# Create output file paths
+
+
+
+
+#### 3.  ####
+
+
+
+
