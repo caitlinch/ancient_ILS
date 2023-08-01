f65d1ba4e67b2b7c9d20a26d1e8065dfa1ccc44f
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-08-01 18:23:07 +1000

    Add branch leading to all animals (extract qcf and monophyly).

--- code/func_analysis.R
+++ code/func_analysis.R
@@ -405,7 +405,8 @@ extract.qcf.values <- function(qcf_tree_path, qcf_log_path){
   
   ## Extract information about the other major clades
   qcf_clade_values <- unlist(lapply(c("Bilateria", "Cnidaria", "Porifera", "Ctenophora", "Outgroup",
-                                      "BilatCnid", "BilatCnidPori", "BilatCnidCten", "CtenPori"), 
+                                      "BilatCnid", "BilatCnidPori", "BilatCnidCten", "CtenPori",
+                                      "Animals"), 
                                     qcf.clade.values, 
                                     qcf_tree))
   
@@ -517,7 +518,11 @@ qcf.clade.values <- function(clade_of_interest, qcf_tree){
                                                       "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70"),
                           "CtenPori_numbers" = c("22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
                                                  "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
-                                                 "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70"))
+                                                 "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70"),
+                          "Animals_numbers" = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21",
+                                                "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                                                "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+                                                "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70"))
   # Identify start and end of branch
   clade_branch_end <- getMRCA( qcf_tree, whelan2017_list[[clade_numbers]] )
   clade_branch_start <- qcf_tree$edge[which(qcf_tree$edge[,2] == clade_branch_end), 1]
