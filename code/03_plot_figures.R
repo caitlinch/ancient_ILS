398c68a8f8803923bcb24c56c27839568862d666
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-08-25 16:19:13 +1000

    Make separate dataframes for investigating the branch lengths for each branch wrt the clades.

--- code/03_plot_figures.R
+++ code/03_plot_figures.R
@@ -55,6 +55,8 @@ names(tree_labs) = c("1", "2")
 hyp_labs <- c("Branch to\nBILAT+CNID+PORI", "Branch to\nBILAT+CNID+CTEN", "Branch to\nCTEN+PORI")
 names(hyp_labs) <- c("Hyp1", "Hyp2", "Hyp3")
 
+
+
 #### Plot 1: Branch length vs summary qCF. ####
 # Create df
 plot1_df <- melt(data = qcf_df,
@@ -136,6 +138,7 @@ p_file <- paste0(output_dir, "results_qcf-score_branch-a-length.png")
 ggsave(filename = p_file, plot = p, device = "png")
 
 
+
 #### Plot 3: Branch length vs branch "b" qCF. ####
 plot3_df <- melt(data = qcf_df,
                  id.vars = c(id.var_cols),
@@ -161,8 +164,8 @@ p <- ggplot(plot3_df, aes(x = branch_b_length, y = value, color = qcf_type)) +
   geom_vline(xintercept = emp_bl[["b"]], linetype = 2, color = "darkgrey") +
   scale_x_continuous(name = "\nLength of branch b (in coalescent units)", trans='log10') +
   scale_y_continuous(name = "qCF Score\n", limits = c(0,1.05), breaks = seq(0,1.1,0.2)) +
-  guides(color = guide_legend(title="qCF type")) +
   scale_color_manual(values = qcf_type_palette, na.value = "grey50") +
+  guides(color = guide_legend(title="qCF type")) +
   theme_bw() +
   theme(strip.text = element_text(size = 15),
         axis.text = element_text(size = 15),
@@ -173,7 +176,8 @@ p_file <- paste0(output_dir, "results_qcf-score_branch-b-length.png")
 ggsave(filename = p_file, plot = p, device = "png")
 
 
-#### Plot 4: Branch length vs clade qCF. ####
+
+#### Plot 4: Branch a length vs clade qCF. ####
 plot4_df <- melt(data = qcf_df,
                  id.vars = c(id.var_cols),
                  measure.vars = c("actual_clade_BILAT_qcf_value", "actual_clade_CNID_qcf_value", "actual_clade_PORI_qcf_value",
@@ -183,22 +187,22 @@ plot4_df <- melt(data = qcf_df,
                                   "estimated_clade_BILAT_CTEN_qcf_value", "estimated_clade_ANIMALS_qcf_value"))
 # Remove any rows with branch a at the empirical branch length (keep only branch b stable for this plot)
 plot4_df <- plot4_df[(plot4_df$branch_a_length != emp_bl[["a"]]),]
-# Format columns for nice plotting
+# Format columns as factors for nice plotting
 plot4_df$clade <- factor(plot4_df$variable,
-                         levels = c("actual_clade_CHOANO_qcf_value", "estimated_clade_CHOANO_qcf_value", 
-                                    "actual_clade_ANIMALS_qcf_value", "estimated_clade_ANIMALS_qcf_value", 
-                                    "actual_clade_BILAT_CTEN_qcf_value", "estimated_clade_BILAT_CTEN_qcf_value",
-                                    "actual_clade_BILAT_qcf_value", "estimated_clade_BILAT_qcf_value", 
-                                    "actual_clade_CNID_qcf_value", "estimated_clade_CNID_qcf_value", 
-                                    "actual_clade_PORI_qcf_value", "estimated_clade_PORI_qcf_value", 
-                                    "actual_clade_CTEN_qcf_value", "estimated_clade_CTEN_qcf_value"), 
-                         labels = c("Choanoflagellates", "Choanoflagellates", 
-                                    "All Animals", "All Animals", 
-                                    "Bilateria+Cnidaria", "Bilateria+Cnidaria",
-                                    "Bilateria", "Bilateria", 
-                                    "Cnidaria", "Cnidaria",
-                                    "Porifera", "Porifera", 
-                                    "Ctenophora", "Ctenophora") )
+                         levels = rev(c("actual_clade_CHOANO_qcf_value", "estimated_clade_CHOANO_qcf_value", 
+                                        "actual_clade_ANIMALS_qcf_value", "estimated_clade_ANIMALS_qcf_value", 
+                                        "actual_clade_BILAT_CTEN_qcf_value", "estimated_clade_BILAT_CTEN_qcf_value",
+                                        "actual_clade_BILAT_qcf_value", "estimated_clade_BILAT_qcf_value", 
+                                        "actual_clade_CNID_qcf_value", "estimated_clade_CNID_qcf_value", 
+                                        "actual_clade_PORI_qcf_value", "estimated_clade_PORI_qcf_value", 
+                                        "actual_clade_CTEN_qcf_value", "estimated_clade_CTEN_qcf_value")), 
+                         labels = rev(c("Choanoflagellates", "Choanoflagellates", 
+                                        "All Animals", "All Animals", 
+                                        "Bilateria+Cnidaria", "Bilateria+Cnidaria",
+                                        "Bilateria", "Bilateria", 
+                                        "Cnidaria", "Cnidaria",
+                                        "Porifera", "Porifera", 
+                                        "Ctenophora", "Ctenophora")) )
 plot4_df$qcf_type <- factor(plot4_df$variable,
                             levels = c("actual_clade_CHOANO_qcf_value", "estimated_clade_CHOANO_qcf_value", 
                                        "actual_clade_ANIMALS_qcf_value", "estimated_clade_ANIMALS_qcf_value", 
@@ -208,20 +212,93 @@ plot4_df$qcf_type <- factor(plot4_df$variable,
                                        "actual_clade_PORI_qcf_value", "estimated_clade_PORI_qcf_value", 
                                        "actual_clade_CTEN_qcf_value", "estimated_clade_CTEN_qcf_value"), 
                             labels = rep(c("Actual", "Estimated"), 7) )
+plot4_df$branch_a_length <- factor(plot4_df$branch_a_length,
+                                   levels = c("1e-08", "1e-07", "1e-06", "1e-05", "1e-04", "0.001", "0.01", "0.1", "1", "10"),
+                                   labels = c("1e-08", "1e-07", "1e-06", "1e-05", "1e-04", "1e-03", "0.01", "0.1", "1", "10"),
+                                   ordered = T)
 
-ggplot(plot4_df, aes(x = branch_a_length, y = value, color = qcf_type)) + 
+p <- ggplot(plot4_df, aes(x = branch_a_length, y = value, color = qcf_type)) + 
   facet_wrap("clade") +
-  geom_point() +
-  scale_x_continuous(name = "\nLength of branch a (in coalescent units)", trans='log10') +
+  geom_boxplot() +
+  scale_x_discrete(name = "\nLength of branch a (in coalescent units)") +
   scale_y_continuous(name = "qCF Score\n", limits = c(0,1.2), breaks = seq(0,1.6,0.2)) +
-  theme_bw()
+  scale_color_manual(values = qcf_type_palette, na.value = "grey50") +
+  guides(color = guide_legend(title="qCF type")) +
+  theme_bw() +
+  theme(strip.text = element_text(size = 20),
+        axis.text = element_text(size = 15),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.title = element_text(size = 30),
+        axis.title.x = element_text(margin = margin(t = 0, r = 0, b = 15, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 0, b = 0, l = 15)),
+        legend.title = element_text(size = 30),
+        legend.text = element_text(size = 25))
+p_file <- paste0(output_dir, "results_clade_branch-a-length.png")
+ggsave(filename = p_file, plot = p, device = "png")
 
-ggplot(plot4_df, aes(x = branch_b_length, y = value, color = qcf_type)) + 
-  facet_grid(clade~hypothesis_tree) +
-  geom_point() +
-  scale_x_continuous(name = "\nLength of branch b (in coalescent units)", trans='log10') +
+
+
+#### Plot 5: Branch b length vs clade qCF. ####
+plot5_df <- melt(data = qcf_df,
+                 id.vars = c(id.var_cols),
+                 measure.vars = c("actual_clade_BILAT_qcf_value", "actual_clade_CNID_qcf_value", "actual_clade_PORI_qcf_value",
+                                  "actual_clade_CTEN_qcf_value", "actual_clade_CHOANO_qcf_value", "actual_clade_BILAT_CTEN_qcf_value",
+                                  "actual_clade_ANIMALS_qcf_value", "estimated_clade_BILAT_qcf_value", "estimated_clade_CNID_qcf_value",
+                                  "estimated_clade_PORI_qcf_value", "estimated_clade_CTEN_qcf_value", "estimated_clade_CHOANO_qcf_value",
+                                  "estimated_clade_BILAT_CTEN_qcf_value", "estimated_clade_ANIMALS_qcf_value"))
+# Remove any rows with branch a at the empirical branch length (keep only branch b stable for this plot)
+plot5_df <- plot5_df[(plot5_df$branch_b_length != emp_bl[["b"]]),]
+# Format columns as factors for nice plotting
+plot5_df$clade <- factor(plot5_df$variable,
+                         levels = rev(c("actual_clade_CHOANO_qcf_value", "estimated_clade_CHOANO_qcf_value", 
+                                        "actual_clade_ANIMALS_qcf_value", "estimated_clade_ANIMALS_qcf_value", 
+                                        "actual_clade_BILAT_CTEN_qcf_value", "estimated_clade_BILAT_CTEN_qcf_value",
+                                        "actual_clade_BILAT_qcf_value", "estimated_clade_BILAT_qcf_value", 
+                                        "actual_clade_CNID_qcf_value", "estimated_clade_CNID_qcf_value", 
+                                        "actual_clade_PORI_qcf_value", "estimated_clade_PORI_qcf_value", 
+                                        "actual_clade_CTEN_qcf_value", "estimated_clade_CTEN_qcf_value")), 
+                         labels = rev(c("Choanoflagellates", "Choanoflagellates", 
+                                        "All Animals", "All Animals", 
+                                        "Bilateria+Cnidaria", "Bilateria+Cnidaria",
+                                        "Bilateria", "Bilateria", 
+                                        "Cnidaria", "Cnidaria",
+                                        "Porifera", "Porifera", 
+                                        "Ctenophora", "Ctenophora")) )
+plot5_df$qcf_type <- factor(plot5_df$variable,
+                            levels = c("actual_clade_CHOANO_qcf_value", "estimated_clade_CHOANO_qcf_value", 
+                                       "actual_clade_ANIMALS_qcf_value", "estimated_clade_ANIMALS_qcf_value", 
+                                       "actual_clade_BILAT_CTEN_qcf_value", "estimated_clade_BILAT_CTEN_qcf_value",
+                                       "actual_clade_BILAT_qcf_value", "estimated_clade_BILAT_qcf_value", 
+                                       "actual_clade_CNID_qcf_value", "estimated_clade_CNID_qcf_value", 
+                                       "actual_clade_PORI_qcf_value", "estimated_clade_PORI_qcf_value", 
+                                       "actual_clade_CTEN_qcf_value", "estimated_clade_CTEN_qcf_value"), 
+                            labels = rep(c("Actual", "Estimated"), 7) )
+plot5_df$branch_a_length <- factor(plot5_df$branch_b_length,
+                                   levels = c("1e-08", "1e-07", "1e-06", "1e-05", "1e-04", "0.001", "0.01", "0.1", "1", "10"),
+                                   labels = c("1e-08", "1e-07", "1e-06", "1e-05", "1e-04", "1e-03", "0.01", "0.1", "1", "10"),
+                                   ordered = T)
+
+p <- ggplot(plot5_df, aes(x = branch_b_length, y = value, color = qcf_type)) + 
+  facet_wrap("clade") +
+  geom_boxplot() +
+  scale_x_discrete(name = "\nLength of branch b (in coalescent units)") +
   scale_y_continuous(name = "qCF Score\n", limits = c(0,1.2), breaks = seq(0,1.6,0.2)) +
-  theme_bw()
+  scale_color_manual(values = qcf_type_palette, na.value = "grey50") +
+  guides(color = guide_legend(title="qCF type")) +
+  theme_bw() +
+  theme(strip.text = element_text(size = 20),
+        axis.text = element_text(size = 15),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.title = element_text(size = 30),
+        axis.title.x = element_text(margin = margin(t = 0, r = 0, b = 15, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 0, b = 0, l = 15)),
+        legend.title = element_text(size = 30),
+        legend.text = element_text(size = 25))
+p_file <- paste0(output_dir, "results_clade_branch-b-length.png")
+ggsave(filename = p_file, plot = p, device = "png")
+
+
+
 
 
 
