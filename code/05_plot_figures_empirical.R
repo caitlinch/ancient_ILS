f0c4e1b8545e9eb2225f35e3ad9a743ba06c569d
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-11-07 17:21:53 +1100

    Add UFb to quartet score plot.

--- code/05_plot_figures_empirical.R
+++ code/05_plot_figures_empirical.R
@@ -83,7 +83,9 @@ ggplot(gcf_df, aes(x = variable_label, y = value, group = variable_label, fill =
   theme(axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")),
         axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 10, unit = "pt")),
         axis.text.x = element_text(angle = 45, vjust = 1.0, hjust = 1.0) )
-
+# Save plot
+gcf_plot_name <- paste0(repo_dir, "figures/", "empirical_gcf_scores.pdf")
+ggsave(filename = gcf_plot_name, plot = gcf_plot, width = 7, height = 8, units = "in")
 
 
 
@@ -92,6 +94,9 @@ ggplot(gcf_df, aes(x = variable_label, y = value, group = variable_label, fill =
 scf_df <- melt(empirical_df,
                id.vars = id.var_cols,
                measure.vars = c("sCF", "sDF1", "sDF2", "sCF_Label"))
+# Save plot
+scf_plot_name <- paste0(repo_dir, "figures/", "empirical_scf_scores.pdf")
+ggsave(filename = scf_plot_name, plot = scf_plot, width = 7, height = 8, units = "in")
 
 
 
@@ -99,21 +104,28 @@ scf_df <- melt(empirical_df,
 # Extract quartet scores into long dataframe
 qs_df <- melt(empirical_df,
               id.vars = id.var_cols,
-              measure.vars = c("quartet_score"))
+              measure.vars = c("quartet_score", "gcf_Label"))
+# Scale UFB from 0 - 1
+qs_df[which(qs_df$variable == "gcf_Label"), ]$value <- qs_df[which(qs_df$variable == "gcf_Label"), ]$value/100
 # Add labels for plotting
 qs_df$branch_label <- factor(qs_df$branch_description,
                              levels = c("To_all_animals", "To_all_other_metazoans", "To_CTEN_clade", "To_PORI_clade"),
                              labels = c("All Animals", "Other Metazoans", "Ctenophora", "Porifera"),
                              ordered = T)
+qs_df$variable_label <- factor(qs_df$variable,
+                               levels = c("quartet_score", "gcf_Label"),
+                               labels = c("Quartet Score", "Ultra-Fast Bootstrap"),
+                               ordered = T)
 qs_df$hypothesis_label <- factor(qs_df$hypothesis_tree,
                                  levels = c("CTEN", "PORI"),
                                  labels = c("Ctenophora-sister", "Porifera-sister"),
                                  ordered = T)
 # Plot quartet scores
 qs_plot <- ggplot(qs_df, aes(x = branch_label, y = value, fill = hypothesis_label)) +
+  facet_grid(.~variable_label, scales = "fixed") +
   geom_boxplot(alpha = 1) +
   scale_x_discrete(name = "Clade") +
-  scale_y_continuous(name = "Quartet score", breaks = seq(0,1,0.1), labels = seq(0,1,0.1), minor_breaks = seq(0,1,0.05)) +
+  scale_y_continuous(name = "Value", breaks = seq(0,1,0.2), labels = seq(0,1,0.2), minor_breaks = seq(0,1,0.05)) +
   scale_fill_manual(name = "Tree topology", values = c( "#E69F00", "#56B4E9"), labels = c("Ctenophora-sister", "Porifera-sister")) +
   theme_bw() +
   theme(axis.title = element_text(size = 18),
@@ -121,9 +133,12 @@ qs_plot <- ggplot(qs_df, aes(x = branch_label, y = value, fill = hypothesis_labe
         axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 10, unit = "pt")),
         axis.text = element_text(size = 14),
         axis.text.x = element_text(angle = 45, vjust = 1.0, hjust = 1.0),
+        strip.text = element_text(size = 18),
         legend.title = element_text(size = 18),
         legend.text = element_text(size = 14),
         legend.key.size = unit(30, "pt") )
+# Save plot
 qs_plot_name <- paste0(repo_dir, "figures/", "empirical_quartet_scores.pdf")
-ggsave(filename = qs_plot_name, plot = qs_plot, width = 7, height = 8, units = "in")
+ggsave(filename = qs_plot_name, plot = qs_plot, width = 10, height = 8, units = "in")
+
 
