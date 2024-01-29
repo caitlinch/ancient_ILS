08260aa34057ed1a303de2b845193ede74772214
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-01-29 15:57:50 +1100

    Copy function to be starting point.

--- code/func_empirical_tree_estimation.R
+++ code/func_empirical_tree_estimation.R
@@ -3,7 +3,7 @@
 # Caitlin Cherryh, 2023
 
 #### TREE ESTIMATION ####
-## Estimating gene trees
+## Estimating gene trees from an alignment
 estimate.empirical.gene.trees.wrapper <- function(row_id, dataframe, iqtree2_path, iqtree2_num_threads = "AUTO", estimate.trees = FALSE){
   ## Wrap around estimate.empirical.gene.trees function
   temp_row <- dataframe[row_id, ]
@@ -222,6 +222,67 @@ estimate.constrained.astral.tree <- function(gene_tree_file, astral_tree_file, a
   # Return ASTRAL command line
   return(command_line)
 }
+
+
+
+## Estimating single gene trees in IQ-Tree
+estimate.empirical.single.gene.tree.wrapper <- function(row_id, dataframe, iqtree2_path, iqtree2_num_threads = "AUTO", estimate.trees = FALSE){
+  ## Wrap around estimate.empirical.gene.trees function
+  temp_row <- dataframe[row_id, ]
+  iqtree_call <- estimate.empirical.gene.trees(alignment_file = temp_row$alignment_file, partition_file = temp_row$partition_file, 
+                                               output_prefix = temp_row$prefix_gene_trees, iqtree2_path = iqtree2_path,
+                                               iqtree2_num_threads = iqtree2_num_threads, model = temp_row$best_model,
+                                               sitefreqs_file = temp_row$PMSF_sitefreq_file, estimate.trees = estimate.trees)
+  return(iqtree_call)
+}
+
+estimate.empirical.single.gene.tree <- function(alignment_file, partition_file, 
+                                                output_prefix, iqtree2_path, 
+                                                iqtree2_num_threads = "AUTO", model = "MFP", 
+                                                sitefreqs_file = NA, estimate.trees = FALSE){
+  ## Estimate a set of gene trees using a partition file
+  ##   Command line: iqtree2 -s ALN_FILE -S PARTITION_FILE --prefix loci -T AUTO
+  
+  # Assemble model command
+  if (is.na(model) == FALSE){
+    model_call <- paste0(" -m ", model)
+  } else if (is.na(model) == TRUE){
+    model_call <- ""
+  }
+  # Collate arguments into command line
+  if (is.na(sitefreqs_file) == TRUE){
+    # Standard model - included in IQ-Tree
+    command_line <- paste0(iqtree2_path, " -s ", alignment_file, " -S ", partition_file, model_call, " -pre ", output_prefix, " -nt ", iqtree2_num_threads)
+  } else {
+    # PMSF model - ssfp file is provided
+    command_line <- paste0(iqtree2_path, " -s ", alignment_file, " -S ", partition_file, model_call, " -fs ", sitefreqs_file, " -pre ", output_prefix, " -nt ", iqtree2_num_threads)
+  }
+  # Call IQ-Tree if desired
+  if (estimate.trees == TRUE){
+    system(command_line)
+  }
+  # Return IQ-Tree command line
+  return(command_line)
+}
+
+
+
+
+## Estimating constrained single gene trees in IQ-Tree
+
+
+
+
+
+## Estimating single gene trees in ASTRAL
+
+
+
+
+
+## Estimating constrained single gene trees in ASTRAL
+
+
 
 
 
