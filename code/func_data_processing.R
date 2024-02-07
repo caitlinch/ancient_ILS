0962e2a4003780def9789c4d5bcf163615f65ff8
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-02-07 17:37:24 +1100

    Correct ELW function to remove columns that were not included in this analysis.

--- code/func_data_processing.R
+++ code/func_data_processing.R
@@ -2,6 +2,7 @@
 ## This script includes functions to analyse concordance factors, quartet scores, and phylogenetic trees
 # Caitlin Cherryh, 2023
 
+library(stringr)
 
 
 #### Reformat output dataframes ####
@@ -16,9 +17,9 @@ summarise.AU.test.results <- function(dataset_id, au_test_df){
   # Pad the dataset df with NA (depending on the number of trees)
   au_test_pvalues <- c(d_df$p_AU, rep(NA, (5 - num_trees)) )
   # Create a new row for output
-  new_row <- c(d_df$dataset[1], d_df$matrix[1], d_df$best_model_code[1], "AU_test_p_values", 
+  new_row <- c(d_df$ID[1], d_df$dataset[1], d_df$matrix[1], d_df$gene[1], "AU_test_p_values", 
                au_test_pvalues, dataset_year)
-  names(new_row) <- c("dataset", "matrix", "best_model_code", "topology_test", 
+  names(new_row) <- c("ID", "dataset", "matrix", "gene", "topology_test", 
                       "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")
   # Return the output
   return(new_row)
@@ -34,11 +35,11 @@ summarise.eLW <- function(dataset_id, au_test_df){
   # Identify the number of trees
   num_trees <- length(d_df$c_ELW)
   # Pad the dataset df with NA (depending on the number of trees)
-  au_test_pvalues <- c(d_df$c_ELW, rep(NA, (5 - num_trees)) )
+  elw_values <- c(d_df$c_ELW, rep(NA, (5 - num_trees)) )
   # Create a new row for output
-  new_row <- c(d_df$dataset[1], d_df$matrix[1], d_df$best_model_code[1], "expected_likelihood_weights", 
-               au_test_pvalues, dataset_year)
-  names(new_row) <- c("dataset", "matrix", "best_model_code", "topology_test", 
+  new_row <- c(d_df$ID[1], d_df$dataset[1], d_df$matrix[1], d_df$gene[1], "expected_likelihood_weights", 
+               elw_values, dataset_year)
+  names(new_row) <- c("ID", "dataset", "matrix", "gene", "topology_test", 
                       "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")
   # Return the output
   return(new_row)
