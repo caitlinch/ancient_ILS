e9d8ed8f59aa40d4e45c4963a55c970cf4352262
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-03-25 13:58:52 +1100

    Function to extract the best model for each gene.

--- code/func_data_processing.R
+++ code/func_data_processing.R
@@ -5,6 +5,7 @@
 library(stringr)
 
 
+
 #### Reformat output dataframes ####
 summarise.AU.test.results <- function(dataset_id, au_test_df){
   ## Function to take one dataset and summarise the AU test results in one row
@@ -24,6 +25,7 @@ summarise.AU.test.results <- function(dataset_id, au_test_df){
   # Return the output
   return(new_row)
 }
+
 
 
 summarise.eLW <- function(dataset_id, au_test_df){
@@ -47,3 +49,136 @@ summarise.eLW <- function(dataset_id, au_test_df){
 
 
 
+#### Extract C60 model parameters ####
+extract.C60.parameters <- function(iqtree_file){
+  # Function that will extract the best model of sequence evolution or the model of sequence evolution used,
+  #   given a .iqtree file
+
+  ## Format required:
+  # LG+C60{...}+F{...}+R{...}
+  
+  # Check if the iqtree file exists
+  if (file.exists(iqtree_file) == TRUE){
+    # If the iqtree_file does exist:
+    ## Open the .iqtree file:
+    iq_lines <- readLines(iqtree_file)
+    
+    ## Extract best fit model
+    bfm_ind <- grep("Best-fit model according to BIC", iq_lines)
+    bfm_str <- gsub(" ", "", strsplit(iq_lines[bfm_ind], ":")[[1]][2])
+    
+    ## Extract mixture model of substitution
+    mm_ind <- grep("Mixture model of substitution", iq_lines)
+    mm_str <- gsub(" ", "", strsplit(iq_lines[mm_ind], ":")[[1]][2])
+    
+    ## Extract table with rates and weights for C60 model
+    # Extract table from iqtree
+    table_start <- intersect(intersect(grep("Component", iq_lines), grep("Rate", iq_lines)), 
+                             intersect(grep("Weight", iq_lines), grep("Parameters", iq_lines)))
+    table_end   <- which(iq_lines == "")[which(which(iq_lines == "") > table_start)][1] - 1
+    table_lines <- iq_lines[table_start:table_end]
+    table_split <- strsplit(table_lines, " ")
+    table_split_filtered <- lapply(table_split[2:length(table_split)], function(x){x[which(x != "")]})
+    table_df    <- as.data.frame(do.call(rbind, table_split_filtered))
+    names(table_df) <- table_split[[1]][which(table_split[[1]] != "")]
+    # Replace any 0 weights with 0.0001 weights
+    replace_weights <- which(as.numeric(table_df$Weight) == 0)
+    table_df$Weight[replace_weights] <- "0.0001"
+    # Create MIX argument sections
+    single_model <- strsplit(table_df[1,]$Component, "\\+")[[1]][1]
+    table_df$component_formatted <- c(single_model,
+                                      paste0(single_model, "+", gsub("F", "", unlist(lapply(strsplit(table_df[2:nrow(table_df),]$Component, "\\+"), function(x){x[[2]]})))) )
+    table_df$component_op <- gsub("1.0000", "1", paste0(table_df$component_formatted, ":", table_df$Rate, ":", table_df$Weight))
+    # Paste MIX argument together
+    mixture_model_str <- paste0("MIX{", paste(table_df$component_op, collapse = ","), "}")
+    
+    ## Extract aa freqs
+    aa_ind              <- grep("State frequencies", iq_lines)
+    aa_emp_counts_check <- grep("empirical counts from alignment", iq_lines)
+    pi_A_check          <- grep("pi\\(A\\)", iq_lines)
+    if (aa_ind == aa_emp_counts_check | identical(pi_A_check, integer(0)) == FALSE){
+      pi_V_check        <- grep("pi\\(V\\)", iq_lines)
+      aa_table_lines    <- iq_lines[pi_A_check:pi_V_check]
+      aa_split          <- strsplit(aa_table_lines, "=")
+      aa_str_raw        <- unlist(lapply(aa_split, function(x){x[[2]]}))
+      aa_str            <- gsub(" ", "", aa_str_raw)
+      aa_op             <- paste(aa_str, collapse = ",")
+    }
+    
+    ## Extract RHAS rates and weights
+    rhas_ind      <- grep("Model of rate heterogeneity", iq_lines)
+    rhas_r        <- as.numeric(str_extract(iq_lines[rhas_ind], "(\\d)+"))
+    rhas_wr_split <- strsplit(grep("Site proportion and rates", iq_lines, value = T), ":")[[1]][2]
+    rhas_wr_raw   <- paste(strsplit(rhas_wr_raw, "\\) \\(")[[1]], collapse = ",")
+    rhas_wr       <- gsub("\\)", "", gsub("\\(", "", gsub(" ", "", rhas_wr_raw)))
+    rhas_op       <- rhas_wr
+    
+    ## Assemble full model
+    full_model <- paste0(mixture_model_str, "+F{", aa_op,"}+R{", rhas_op, "}")
+  } else if (file.exists(iqtree_file) == FALSE){
+    # If the iqtree_file doesn't exist, return NA
+    full_model = NA
+  } # end if (file.exists(iqtree_file) == TRUE){
+  
+  # Return C60 parameters in model format
+  return(full_model)
+}
+
+
+
+extract.C60.alias <- function(iqtree_log, iqtree_file){
+  # Function that will extract the best model of sequence evolution or the model of sequence evolution used,
+  #   given a .log file
+  
+  # Check if the log file exists
+  if (file.exists(iqtree_log) == TRUE & file.exists(iqtree_file) == TRUE){
+    # If the iqtree_file does exist:
+    ## Open the .iqtree file:
+    log_lines <- readLines(iqtree_log)
+    iq_lines  <- readLines(iqtree_file)
+    
+    # Extract best-fit model
+    bfm_line    <- grep("Best-fit model", log_lines, value = T)
+    best_model  <- strsplit(bfm_line, " ")[[1]][3]
+    best_model_chunks <- strsplit(best_model, "\\+")[[1]]
+    
+    ## Extract alias model
+    alias_lines <- grep("alias", log_lines, value = T)
+    best_model_alias_line <- alias_lines[which(duplicated(alias_lines) == TRUE)]
+    alias_line_split <- strsplit(best_model_alias_line, " is alias for ")[[1]]
+    alias_model <- alias_line_split[2]
+    alias_split <- strsplit(alias_model, "\\+")[[1]]
+    fmix_term <- grep("FMIX", alias_split, value = T)
+    
+    ## Extract rates and weights
+    rhas_lines  <- grep("Site proportion and rates", log_lines, value = TRUE)
+    rhas_line   <- rhas_lines[length(rhas_lines)]
+    rhas_split  <- strsplit(strsplit(rhas_line, ":")[[1]][2], "\\) \\(")[[1]]
+    rhas_clean  <- gsub("\\)", "", gsub("\\(", "", gsub(" ", "", rhas_split)))
+    rhas        <- paste(rhas_clean, collapse = ",")
+    
+    ## Extract aa freqs
+    pi_A_check          <- grep("pi\\(A\\)", iq_lines)
+    if (identical(pi_A_check, integer(0)) == FALSE){
+      pi_V_check        <- grep("pi\\(V\\)", iq_lines)
+      aa_table_lines    <- iq_lines[pi_A_check:pi_V_check]
+      aa_split          <- strsplit(aa_table_lines, "=")
+      aa_str_raw        <- unlist(lapply(aa_split, function(x){x[[2]]}))
+      aa_str            <- gsub(" ", "", aa_str_raw)
+      aa_op             <- paste(aa_str, collapse = ",")
+      aa_freqs          <- paste0("{", aa_op, "}")
+    } else {
+      aa_freqs <- ""
+    }
+    
+    ## Assemble the model back together
+    full_model <- paste0(best_model_chunks[[1]], "+", fmix_term, "+", best_model_chunks[[3]], "+", best_model_chunks[[4]], "{", rhas, "}")
+    
+  } else if (file.exists(iqtree_log) == FALSE){
+    # If the iqtree_log doesn't exist, return NA
+    full_model = NA
+  } # end if (file.exists(iqtree_log) == TRUE){
+  
+  # Return C60 parameters in model format
+  return(full_model)
+}
