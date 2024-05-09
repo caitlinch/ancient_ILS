225a29e7f0a0c0d76001d4511baa7f68b2a12a1c
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-05-09 15:39:10 +1000

    New folder for functions to extract concordance factors from gcf/qcf output.

---
+++ code/func_concordance_factors.R
@@ -0,0 +1,9 @@
+# ancient_ILS/code/func_concordance_factors.R
+## This script includes functions to extract concordance factors for different datasets
+# Caitlin Cherryh, 2023
+
+library(ape)
+library(castor)
+
+
+
