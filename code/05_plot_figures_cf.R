14099a7517e0a1394ac08b3f6c1c5b6cf0cf2b09
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-05-20 12:42:24 +1000

    Open all dataframes and format identically for plotting columns to ease ggplot scripts.

--- code/05_plot_figures_cf.R
+++ code/05_plot_figures_cf.R
@@ -9,7 +9,9 @@
 #   For PORI-Tree: "All other animals"
 #   For CTEN-PORI-Tree: "CTEN+PORI
 
-###### 1. Input parameters ######
+
+
+##### 1. Input parameters #####
 ## Specify parameters:
 # repo_dir          <- Location of caitlinch/metazoan-mixtures github repository
 # output_csv_dir    <- Location of csv input/output/results files
@@ -21,17 +23,17 @@ plot_dir          <- "/Users/caitlincherryh/Documents/C4_Ancient_ILS/08_figures/
 
 
 
-###### 2. Open packages and functions ######
+##### 2. Open packages and functions #####
 ## Open packages
 library(reshape2)
 library(ggplot2)
 library(ggtern) # for ternary plots of sCF
 library(patchwork) # for assembling ternary plots of emperical gCF, sCF and quartet scores
 
-# Source functions
+## Source functions
 source(paste0(repo_dir, "code/func_plotting.R"))
 
-# Specify colour palettes used within these plots
+## Specify colour palettes used within these plots
 metazoan_clade_palette  <- c(Bilateria = "#CC79A7", Cnidaria = "#009E73", Ctenophora = "#56B4E9", Porifera = "#E69F00", Outgroup = "#999999")
 clades_colours          <- c("Ctenophora" = "#2171b5", "Porifera" =  "#E69F00", "Ctenophora+Porifera" = "#009E73")
 clades_black            <- c("Ctenophora" = "black", "Porifera" =  "black", "Ctenophora+Porifera" = "black")
@@ -39,151 +41,71 @@ passfail_palette        <- c("Pass (p > 0.05)" = "#addd8e", "Fail (p \u2264 0.05
 
 
 
-###### 3. Open and prepare csvs for plotting  ######
-# List all output files
-all_output_files <- list.files(output_dir)
+##### 3. Open and prepare csvs for plotting  #####
+## List all output files
+all_output_files <- paste0(repo_dir, "output/", list.files(output_csv_dir))
 
-# Open sCF results and remove unused datasets
-species_scf_df  <- read.csv(paste0(repo_dir, "output/empirical_dataset_concordance_factors.csv"),  stringsAsFactors = FALSE)
-species_scf_df  <- species_scf_df[which(species_scf_df$dataset != "Simion2017"), ]
-species_scf_df  <- species_scf_df[which(species_scf_df$dataset != "Hejnol2009"), ]
-
-# Open constrained/unconstrained cf results
-cf_df <- read.csv(paste0(output_dir, grep("constrained_unconstrained_cf_output_checked.csv", all_output_files, value = T)), stringsAsFactors = F)
-# Remove any Plac-sister branches (these do not include the branch of interest)
-cf_df <- cf_df[grep("PlacSister", cf_df$branch_description, invert = T), ]
+## Open empirical cf analysis results
+gcf_df    <- read.csv(grep("gCF_values.csv", all_output_files, value = TRUE))
+gcf_long  <- read.csv(grep("gCF_values_formatted.csv", all_output_files, value = TRUE))
+qcf_df    <- read.csv(grep("qCF_values.csv", all_output_files, value = TRUE))
+qcf_long  <- read.csv(grep("qCF_values_formatted.csv", all_output_files, value = TRUE))        
 
 
 
-###### 4. Update and format dataframes for plots ######
-# Add any missing columns to species_scf_df
-species_scf_df$dataset_id <- paste0(species_scf_df$dataset, ".", species_scf_df$matrix) 
-species_scf_df$dataset_id_formatted <- factor(species_scf_df$dataset_id,
-                                              levels =  c("Dunn2008.Dunn2008_FixedNames", "Philippe2009.Philippe_etal_superalignment_FixedNames", "Philippe2011.UPDUNN_MB_FixedNames", 
-                                                          "Nosenko2013.nonribosomal_9187_smatrix", "Nosenko2013.ribosomal_14615_smatrix", "Ryan2013.REA_EST_includingXenoturbella", 
-                                                          "Moroz2014.ED3d", "Borowiec2015.Best108", "Chang2015.Chang_AA", 
-                                                          "Whelan2015.Dataset10", "Whelan2017.Metazoa_Choano_RCFV_strict", "Laumer2018.Tplx_BUSCOeuk"),
-                                              labels = c("Dunn 2008",  "Philippe 2009", "Philippe 2011", 
-                                                         "Nosenko 2013\nnonribosomal", "Nosenko 2013\nribosomal", "Ryan 2013", 
-                                                         "Moroz 2014", "Borowiec 2015", "Chang 2015", 
-                                                         "Whelan 2015", "Whelan 2017",  "Laumer 2018"),
-                                              ordered = TRUE)
-species_scf_df$tree_topology <- species_scf_df$hypothesis_tree
-species_scf_df$tree_topology_formatted <- factor(species_scf_df$tree_topology,
-                                                 levels =  c("CTEN", "PORI", "CTEN_PORI"),
-                                                 labels = c("Ctenophora", "Porifera", "Ctenophora+Porifera"),
-                                                 ordered = TRUE)
-species_scf_df$branch_to_clade <- factor(species_scf_df$branch_description,
-                                         levels = c("To_all_animals", "To_all_other_metazoans", "To_CTEN_clade", "To_PORI_clade",
-                                                    "To_CTEN+PLAC_clade", "To_PORI+PLAC_clade", "To_all_animals_except_PLAC"),
-                                         labels = c("ALL_ANIMALS", "ALL_OTHER_ANIMALS", "CTEN", "PORI",
-                                                    "CTEN_PLAC", "PORI_PLAC", "ALL_ANIMLS_EXCEPT_PLAC"),
-                                         ordered = TRUE)
-species_scf_df$ultrafast_bootstrap <- species_scf_df$sCF_Label
-# Add any missing columns to cf_df
-cf_df$dataset_id <- paste0(cf_df$dataset, ".", cf_df$matrix) 
-cf_df$dataset_id_formatted <- factor(cf_df$dataset_id,
-                                     levels =  c("Dunn2008.Dunn2008_FixedNames", "Philippe2009.Philippe_etal_superalignment_FixedNames", "Philippe2011.UPDUNN_MB_FixedNames", 
-                                                 "Nosenko2013.nonribosomal_9187_smatrix", "Nosenko2013.ribosomal_14615_smatrix", "Ryan2013.REA_EST_includingXenoturbella", 
-                                                 "Moroz2014.ED3d", "Borowiec2015.Best108", "Chang2015.Chang_AA", 
-                                                 "Whelan2015.Dataset10", "Whelan2017.Metazoa_Choano_RCFV_strict", "Laumer2018.Tplx_BUSCOeuk"),
-                                     labels = c("Dunn 2008",  "Philippe 2009", "Philippe 2011", 
-                                                "Nosenko 2013\nnonribosomal", "Nosenko 2013\nribosomal", "Ryan 2013", 
-                                                "Moroz 2014", "Borowiec 2015", "Chang 2015", 
-                                                "Whelan 2015", "Whelan 2017",  "Laumer 2018"),
-                                     ordered = TRUE)
-cf_df$tree_topology <- cf_df$hypothesis_tree
-cf_df$tree_topology_formatted <- factor(cf_df$tree_topology,
-                                        levels =  c("CTEN", "PORI", "CTEN_PORI"),
-                                        labels = c("Ctenophora", "Porifera", "Ctenophora+Porifera"),
-                                        ordered = TRUE)
-cf_df$branch_to_clade <- factor(cf_df$branch_description,
-                                levels = c("Metazoa", "All_other_animals", "CTEN_PORI"),
-                                labels = c("ALL_ANIMALS", "ALL_OTHER_ANIMALS", "CTEN_PORI"),
-                                ordered = TRUE)
-cf_df$clade_formatted <- factor(cf_df$branch_description,
-                                levels = c("Metazoa", "All_other_animals", "CTEN_PORI"),
-                                labels = c("Metazoa", "All other animals", "Ctenophora+Porifera"),
-                                ordered = TRUE)
-cf_df$ultrafast_bootstrap <- cf_df$gcf_Label
-cf_df$gene_type_formatted <- factor(cf_df$gene_type,
-                                    levels = c("Unconstrained", "Constrained"),
-                                    labels = c("Unconstrained genes", "Constrained genes"),
-                                    ordered = T)
+##### 4. Update and format dataframes for plots #####
+## Add nicely formatted dataset labels to the dataframes
+dataset_labels                <- c("Dunn2008.Dunn2008_FixedNames", "Philippe2009.Philippe_etal_superalignment_FixedNames", "Philippe2011.UPDUNN_MB_FixedNames", 
+                                   "Nosenko2013.nonribosomal_9187_smatrix", "Nosenko2013.ribosomal_14615_smatrix", "Ryan2013.REA_EST_includingXenoturbella", 
+                                   "Moroz2014.ED3d", "Borowiec2015.Best108", "Chang2015.Chang_AA", 
+                                   "Whelan2015.Dataset10", "Whelan2017.Metazoa_Choano_RCFV_strict", "Laumer2018.Tplx_BUSCOeuk")
+dataset_labels_formatted      <- c("Dunn 2008",  "Philippe 2009", "Philippe 2011", 
+                                   "Nosenko 2013\nnonribosomal", "Nosenko 2013\nribosomal", "Ryan 2013", 
+                                   "Moroz 2014", "Borowiec 2015", "Chang 2015", 
+                                   "Whelan 2015", "Whelan 2017",  "Laumer 2018")
+gcf_df$dataset_id_formatted   <- factor(gcf_df$dataset_id, levels =  dataset_labels, labels = dataset_labels_formatted, ordered = TRUE)
+gcf_long$dataset_id_formatted <- factor(gcf_long$dataset_id, levels =  dataset_labels, labels = dataset_labels_formatted, ordered = TRUE)
+qcf_df$dataset_id_formatted   <- factor(qcf_df$dataset_id, levels =  dataset_labels, labels = dataset_labels_formatted, ordered = TRUE)
+qcf_long$dataset_id_formatted <- factor(qcf_long$dataset_id, levels =  dataset_labels, labels = dataset_labels_formatted, ordered = TRUE)
+
+## Add nicely formatted topology labels to the dataframes
+topology_labels                   <- c("CTEN", "PORI", "CTEN_PORI")
+topology_labels_formatted         <- c("Ctenophora", "Porifera", "Ctenophora+Porifera")
+topology_labels_short             <- c("CTEN", "PORI", "CTEN+PORI")
+gcf_df$tree_topology_formatted    <- factor(gcf_df$tree_topology, levels =  topology_labels, labels = topology_labels_formatted, ordered = TRUE)
+qcf_df$tree_topology_formatted    <- factor(qcf_df$topology, levels =  topology_labels, labels = topology_labels_formatted, ordered = TRUE)
+gcf_df$tree_topology_short        <- factor(gcf_df$tree_topology, levels =  topology_labels, labels = topology_labels_short, ordered = TRUE)
+qcf_df$tree_topology_short        <- factor(qcf_df$topology, levels =  topology_labels, labels = topology_labels_short, ordered = TRUE)
+
+## Add nicely formatted PLACOZOA label to the dataframes
+Plac_labels                     <- c("Plac", "noPlac")
+Plac_labels_formatted           <- c("Present", "Removed")
+gcf_df$Plac_present_formatted   <- factor(gcf_df$Plac_present, levels =  Plac_labels, labels = Plac_labels_formatted, ordered = TRUE)
+gcf_long$Plac_present_formatted <- factor(gcf_long$Plac_present, levels =  Plac_labels, labels = Plac_labels_formatted, ordered = TRUE)
+qcf_df$Plac_present_formatted   <- factor(qcf_df$Plac_present, levels =  Plac_labels, labels = Plac_labels_formatted, ordered = TRUE)
+qcf_long$Plac_present_formatted <- factor(qcf_long$Plac_present, levels =  Plac_labels, labels = Plac_labels_formatted, ordered = TRUE)
+
+## Add nicely formatted column describing analysis
+gcf_df$analysis   <- "gCF"
+gcf_long$analysis <- "gCF"
+qcf_df$analysis   <- "qCF"
+qcf_long$analysis <- "qCF"
 
 
 
-###### 4. Species sCF df: Empirical gCF, sCF and quartet values, All Other Metazoans, 2 Topologies ######
-# Extract variables of interest
-emp2_df <- species_scf_df
-emp2_df <- emp2_df[which(emp2_df$branch_to_clade %in% c("ALL_ANIMALS", "ALL_OTHER_ANIMALS", "CTEN", "PORI")), ]
-# Add new columns
-emp2_df$clade_formatted <- factor(emp2_df$branch_to_clade,
-                                  levels = c("ALL_ANIMALS", "ALL_OTHER_ANIMALS", "CTEN", "PORI"),
-                                  labels = c("Metazoa", "All other animals", "Ctenophora", "Porifera"))
-# Plot gCF
-emp2_gcf <- ggtern(emp2_df, aes(x = gDF1, y = gCF, z = gDF2, color = tree_topology_formatted, shape = tree_topology_formatted)) +
-  geom_point(size = 4, alpha = 0.6) +
-  facet_wrap(clade_formatted ~., nrow = 1, ncol = 4) +
-  labs(title = "a.") +
-  scale_color_manual(values = clades_colours, name = "Constrained\ntree topology") +
-  scale_shape_manual(values = c("Ctenophora" = 16, "Porifera" = 17), name = "Constrained\ntree topology") +
-  theme_bw() +
-  theme(plot.title = element_text(size = 30),
-        strip.text = element_text(size = 20),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 14),
-        legend.key.size = unit(1.5, "lines"),
-        tern.panel.grid.major = element_line(colour = "grey80", linewidth = 8),
-        tern.panel.grid.minor = element_line(colour = "grey80", linewidth = 8) ) +
-  guides( color = guide_legend(override.aes = list(size = 5)) )  
-# Plot sCF
-emp2_scf <- ggtern(emp2_df, aes(x = sDF1, y = sCF, z = sDF2, color = tree_topology_formatted, shape = tree_topology_formatted)) +
-  geom_point(size = 4, alpha = 0.6) +
-  facet_wrap(clade_formatted ~., nrow = 1, ncol = 4) +
-  labs(title = "b.") +
-  scale_color_manual(values = clades_colours, name = "Constrained\ntree topology") +
-  scale_shape_manual(values = c("Ctenophora" = 16, "Porifera" = 17), name = "Constrained\ntree topology") +
-  theme_bw() +
-  theme(plot.title = element_text(size = 30),
-        strip.text = element_text(size = 20),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 14),
-        legend.key.size = unit(1.5, "lines"),
-        tern.panel.grid.major = element_line(colour = "grey80", linewidth = 8),
-        tern.panel.grid.minor = element_line(colour = "grey80", linewidth = 8) ) +
-  guides( color = guide_legend(override.aes = list(size = 5)) ) 
-# Plot quartet scores
-emp2_qs <- ggtern(emp2_df, aes(x = q2, y = q1, z = q3, color = tree_topology_formatted, shape = tree_topology_formatted)) +
-  geom_point(size = 4, alpha = 0.6) +
-  facet_wrap(clade_formatted ~., nrow = 1, ncol = 4) +
-  labs(title = "c.") +
-  scale_color_manual(values = clades_colours, name = "Constrained\ntree topology") +
-  scale_shape_manual(values = c("Ctenophora" = 16, "Porifera" = 17), name = "Constrained\ntree topology") +
-  theme_bw() +
-  theme(plot.title = element_text(size = 30),
-        strip.text = element_text(size = 20),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 14),
-        legend.key.size = unit(1.5, "lines"),
-        tern.panel.grid.major = element_line(colour = "grey80", linewidth = 8),
-        tern.panel.grid.minor = element_line(colour = "grey80", linewidth = 8) ) +
-  guides( color = guide_legend(override.aes = list(size = 5)) ) +
-  Tlab("qCF") +
-  Llab("qDF1") +
-  Rlab("qDF2")
-# Assemble the three ternary plots using ggtern::grid.arrange 
-#     (as ternary plots have three axes, patchwork and ggplot::grid.arrange don't work cleanly here)
-quilt2 <- ggtern::grid.arrange(emp2_gcf, emp2_scf, emp2_qs, nrow = 3, ncol = 1)
-# Save the quilt
-emp_tern_file <- paste0(repo_dir, "figures/", "empirical_dataset_gCF_sCF_qs_ternary_plots_coloured.pdf")
-ggsave(filename = emp_tern_file, plot = quilt2, width = 18, height = 12, units = "in")
-emp_tern_file_png <- paste0(repo_dir, "figures/", "empirical_dataset_gCF_sCF_qs_ternary_plots_coloured.png")
-ggsave(filename = emp_tern_file_png, plot = quilt2, width = 18, height = 12, units = "in")
+##### 5. gene concordance factors (gCF) ternary plots #####
 
 
 
-###### 4. CF df: Empirical gCF, sCF and quartet values, Key branch, 3 Topologies ######
+
+##### 6. quartet concordance factors (qCF) ternary plots #####
+
+
+
+
+
+#################### OLD CODE
+##### 4. CF df: Empirical gCF, sCF and quartet values, Key branch, 3 Topologies #####
 # Extract variables of interest
 emp3_df <- cf_df
 # Remove any rows that are NOT leading to the "key branch"
@@ -270,7 +192,7 @@ ggsave(filename = emp3_tern_file_png, plot = quilt3, width = 10, height = 12, un
 
 
 
-###### 5. CF + Key branch + 3 topologies + unconstrained gene trees ######
+##### 5. CF + Key branch + 3 topologies + unconstrained gene trees #####
 # Key branch to extract:
 #   CF = CTEN-Tree, "All other animals"
 #   DF1 = PORI-Tree, "All other animals"
